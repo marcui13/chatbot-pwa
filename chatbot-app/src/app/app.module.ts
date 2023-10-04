6fcfba5908f8a3647f9e3da2503d2186b7c11d21
Agustín <amarquardt@movizen.com>
2023-10-04 20:15:58 -0300

    start frontend

---
+++ chatbot-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ChatbotComponent } from './chatbot/chatbot.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatbotComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
